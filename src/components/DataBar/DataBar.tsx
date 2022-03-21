193a0f80a581fad84b62af1a674ccabb73e9b1f0
Trevor <tkhowell95@gmail.com>
2022-03-20 19:29:16 -0600

    Added scss preprocessor

--- src/components/DataBar/DataBar.tsx
+++ src/components/DataBar/DataBar.tsx
@@ -1,54 +1,54 @@
 import { motion } from 'framer-motion';
 import React from 'react';
-import './DataBar.css';
+import './DataBar.scss';
 
 interface DataBarArgs {
-  /**
-   * A number between 0 and 1 representing
-   * the height value, with 0 being no height
-   * and 1 being 100% height.
-   */
-  value: number;
-  /**
-   * A unique identifier to differentiate this
-   * object from other DataBar objects.
-   */
-  uniqueId: string;
-  /**
-   * Renders over the bar if provided.
-   */
-  text?: string;
-  /**
-   * The number of pixels wide this DataBar
-   * should be. Leave empty for auto-width.
-   */
-  width?: number;
-  color?: string;
-  index?: number;
-  animationDuration?: number;
+	/**
+	 * A number between 0 and 1 representing
+	 * the height value, with 0 being no height
+	 * and 1 being 100% height.
+	 */
+	value: number;
+	/**
+	 * A unique identifier to differentiate this
+	 * object from other DataBar objects.
+	 */
+	uniqueId: string;
+	/**
+	 * Renders over the bar if provided.
+	 */
+	text?: string;
+	/**
+	 * The number of pixels wide this DataBar
+	 * should be. Leave empty for auto-width.
+	 */
+	width?: number;
+	color?: string;
+	index?: number;
+	animationDuration?: number;
 }
 
 const DataBar = (args: DataBarArgs) => {
-  return (
-    <motion.div
-      layoutId={args.uniqueId}
-      className="data-bar"
-      transition={{ duration: args.animationDuration || 0.25, type: 'spring' }}
-    >
-      <div className="fill-area" style={getStyle(args)}>
-        {args.text}
-      </div>
-    </motion.div>
-  );
+	return (
+		<motion.div
+			layoutId={args.uniqueId}
+			className="data-bar"
+			transition={{ duration: args.animationDuration || 0.25, type: 'spring' }}
+		>
+			<div className="fill-area" style={getStyle(args)}>
+				{args.text}
+			</div>
+		</motion.div>
+	);
 };
 
 const getStyle = ({ color = 'steelblue', width, value }: DataBarArgs): React.CSSProperties => {
-  value = Math.max(Math.min(value, 1), 0);
-  return {
-    backgroundColor: color,
-    height: (value * 100).toPrecision(2) + '%',
-    width: (width && `${width}px`) || '95%',
-  };
+	value = Math.max(Math.min(value, 1), 0);
+	return {
+		backgroundColor: color,
+		height: (value * 100).toPrecision(2) + '%',
+		width: (width && `${width}px`) || '95%',
+	};
 };
 
 export default DataBar;
